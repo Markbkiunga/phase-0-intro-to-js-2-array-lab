d5e340e5cf91c04dfb626fb94a136583c71e4ddf
Mark Brian <markbkiunga2017@gmail.com>
2024-06-22 10:48:33 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+  console.log(cats);
+  return cats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+  console.log(cats);
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  console.log(cats);
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  console.log(cats);
+}
+function appendCat(name) {
+  const newCats = [...cats, "Broom"];
+  console.log(newCats);
+  return newCats;
+}
+function prependCat(name) {
+  const newCats = ["Arnold", ...cats];
+  console.log(newCats);
+  return newCats;
+}
+function removeLastCat() {
+  const newCats = [...cats.slice(0, -1)];
+  console.log(newCats);
+  return newCats;
+}
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  console.log(newCats);
+  return newCats;
+}
